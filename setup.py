c718271b806d8fe488432f31b8a8bf788aaeacf9
qutang <tqshelly@gmail.com>
2018-07-09 16:29:23 -0400

    Update version and gitignore

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='padar_converter',
-    version='0.1.0',
+    version='0.2.0',
     packages=find_packages(),
     include_package_data=True,
     description='Extension of converters for sensor data used in padar package',
